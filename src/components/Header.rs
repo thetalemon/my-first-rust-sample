23625f4d48d0d6330f239072f2a1378b1a172d58
thetalemon <sasami0314@gmail.com>
2023-06-04 23:27:39 +0900

    再調整

--- src/components/Header.rs
+++ src/components/Header.rs
@@ -1,1 +1,12 @@
-pub mod header;
+use yew::{function_component, html, Html};
+
+#[function_component(Header)]
+pub fn header() -> Html {
+    html! {
+      <header class="p-2 color-bg-accent">
+        <div>
+          <a  href="#">{"Yew TODO App"}</a>
+        </div>
+      </header>
+    }
+}
